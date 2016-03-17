712651ccde190fdbb794017d725b5e0a2a08bb19
川流不息 <1760002043@qq.com>
2016-03-17 19:56:04 +0800

    updata

---
+++ PWM.c
@@ -0,0 +1,16 @@
+/*
+ *MSP430F5529 P2.0Êä³ö25%PWM
+ */
+ #include <msp430f5529.h>
+
+void main()
+{
+	WDTCTL=WDTPW+WDTHOLD;
+	P2DIR|=BIT0;
+	P2SEL|=BIT0;
+	TA1CCTL1=OUTMOD_3;
+	TA1CCR0=16484;
+	TA1CCR1=100;
+	TA1CTL=TASSEL_1+MC_2+TACLR;
+	__bis_SR_register(LPM3_bits);
+}
